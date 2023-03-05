c549815a7c26d818c6a10d7d9831776b1721e1d4
Jalil David Salamé Messina <jalil.salame@tum.de>
2023-03-05 19:26:35 +0100

    feat: Add Term and TermID

--- librsat/src/lib.rs
+++ librsat/src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// A single term (ie. x₁, ¬x₁, etc)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Term(u64);
+
+/// A term identifier: TermID(x₁) == TermID(¬x₁)
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct TermID(u64);
+
+impl TermID {
+    /// Turn back into a term
+    /// x₁.into::<TermID>().literal() == x₁
+    pub fn literal(self) -> Term {
+        Term(self.0 << 1)
+    }
+
+    /// Turn back into a negated term
+    /// x₁.into::<TermID>().literal() == ¬x₁
+    pub fn negate(self) -> Term {
+        Term((self.0 << 1) | 1)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl From<Term> for TermID {
+    fn from(value: Term) -> Self {
+        TermID(value.0 >> 1)
     }
 }
